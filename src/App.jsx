9366aa035efc78fba5dee3593372e2ca63941630
lautarohelp <lautarotoledo391@gmail.com>
2025-08-12 13:02:53 -0300

    refactor: update App component to use useScrollToTop hook and adjust import paths for consistency; comment out background color in CSS

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from './page/Home'
 import OpenLabs from './components/Outreach/sections/openLabs/OpenLabs'
 import Grasp from './components/Outreach/sections/grasp/grasp'
-import Tell from './components/Outreach/sections/tell/tell'
-import { useScrollPosition } from './utils/useScrollPosition'
+import Tell from './components/Outreach/sections/tell/Tell'
+import { useScrollToTop } from './components/useScrollToTop'
 
 function App() {
-  // Use the scroll position hook to maintain scroll position across reloads
-  useScrollPosition();
+  // Scroll to top on route change
+  useScrollToTop();
 
   return (
   <Routes>
